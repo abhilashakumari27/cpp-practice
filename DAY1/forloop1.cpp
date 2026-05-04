d2e272c729b37e7e87a1bf6de0e1314ce80f37ef
Abhilasha Kumari <abhilashakumati@gmail.com>
2026-05-04 17:37:49 +0530

    first commit

--- DAY1/forloop1.cpp
+++ DAY1/forloop1.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    int n = 5;
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
     
     for (int i =1;i<=n;i++){
         for (int j =1;j<=i;j++){
