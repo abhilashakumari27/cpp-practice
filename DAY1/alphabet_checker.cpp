39b07b2d7a42b0afb3bfb935068a700d18a65823
Abhilasha Kumari <abhilashakumati@gmail.com>
2026-05-07 19:15:08 +0530

    Normal commit

---
+++ DAY1/alphabet_checker.cpp
@@ -0,0 +1,17 @@
+/*
+Problem 10: Alphabet Case Checker
+Write a C++ program that takes a character as input.
+Determine whether it is uppercase, lowercase, or not an alphabet. 
+Use relational operators to check the character range and print the appropriate message.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin >> ch;
+
+    bool is upper
+}
