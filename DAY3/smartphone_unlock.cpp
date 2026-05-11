c2f919a61e98ff8cf684a69974fceb21fe8cb464
Abhilasha Kumari <abhilashakumati@gmail.com>
2026-05-11 19:23:19 +0530

    Normal commit

---
+++ DAY3/smartphone_unlock.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int passcode;
+
+    do
+       
+    {
+        cin >> passcode;
+    } while(passcode != 1111);
+
+    cout << "Phone Unlocked!" << endl;
+    return 0;
+        
+    
+    
+}
