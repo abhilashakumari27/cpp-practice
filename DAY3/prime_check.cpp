dd10747af75995300fd277cebc4b3dbcb1c6ebbb
Abhilasha Kumari <abhilashakumati@gmail.com>
2026-05-11 19:36:36 +0530

    Normal commit

---
+++ DAY3/prime_check.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    bool isPrime = true;
+
+    if (n <= 1) {
+        isPrime = false;
+
+        for(int i = 2; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime) 
+        cout <<  "prime";
+    else
+        cout <<"Not a prime ";
+
+
+    return 0;
+}
