344ecf2046f43761002e8286966c61e666d9206d
Abhilasha Kumari <abhilashakumati@gmail.com>
2026-05-13 08:28:07 +0530

    Normal commit

---
+++ DAY13/sumofnumbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+    int fun( int n)
+    {
+        if (n == 0) 
+        return 0;
+
+        return (n % 10) + fun(n/10);
+
+    }
+    int main()
+    {
+
+    int n;
+    cin >> n;
+    cout << "Sum of digits:"<< fun(n);
+    return 0;
+
+}
